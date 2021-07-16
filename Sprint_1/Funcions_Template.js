2dd90ce17695abefe7396dd1a2835ec328e2ad1e
Eloielbonnoi <57134772+Eloielbonnoi@users.noreply.github.com>
2021-07-16 11:56:18 +0200

    Update Funcions_Template.js

--- Sprint_1/Funcions_Template.js
+++ Sprint_1/Funcions_Template.js
@@ -1,17 +1,17 @@
-//Nivell 1 Crear una funció que imprimeixi en consola el nom d'usuari sent invocada externament i passant el nom com a paràmetre.
+//Nivell 1 Crear una funció que imprimeixi en consola el nom d'usuari sent invocada externament i passant el nom com a paràmetre.//RESOLT
 
 let userName;
 const pasName = (userName)=> console.log(userName);
 pasName('Eloi');
 
 //Nivell 2.1
-//Imprimir el nom i cognoms de l'usuari en variables mitjançant template literals, creant les variables i referenciant en la impressió de l'missatge
+//Imprimir el nom i cognoms de l'usuari en variables mitjançant template literals, creant les variables i referenciant en la impressió de l'missatge //RESOLT
      
 let nom ='Eloi';
 let cognom = 'Isern';
 console.log( `El teu nom és ${ nom } i el cognom ${cognom}`);
 
-//Nivell 2.2 Invocar la funció mitjançant template literals.
+//Nivell 2.2 Invocar la funció mitjançant template literals. //RESOLT
 
 let userName; //D'això se'n pot prescindir
 const pasName = (userName)=> userName;
@@ -20,7 +20,7 @@ console.log(`This is ${pasName('Eloi')}`);
 
 //Nivell 3.1 Crea una matriu de deu funcions i empleni-la mitjançant un bucle. Cada funció comptarà del 0-9 imprimint-ho per pantalla. 
 //Invoqui cada funció de l'array iterativament. Haurà d'imprimir-se per pantalla el compte de 0-9 deu vegades
-//Exemple amb 3 funcions, però és exactament igual amb 10.
+//Exemple amb 3 funcions, però és exactament igual amb 10.//RESOLT PERÒ AMB DUBTES
 
   function count1() {
     for (var i = 1; i<10; i++){
@@ -56,7 +56,7 @@ for (i=1; i <10; i++){
 };
 */
 
-//Nivell 3.2 Crear una funció anònima autoinvocable (igualada a una variable) que imprimeixi per pantalla el nom d'usuari rebut com a paràmetre
+//Nivell 3.2 Crear una funció anònima autoinvocable (igualada a una variable) que imprimeixi per pantalla el nom d'usuari rebut com a paràmetre. //RESOLT
 
 var printConsole = (function(userName){
     console.log(userName);
