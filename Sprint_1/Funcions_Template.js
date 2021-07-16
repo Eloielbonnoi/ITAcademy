ef4d1f4c5a8895107d355140c4011b6cecac28ce
Eloielbonnoi <57134772+Eloielbonnoi@users.noreply.github.com>
2021-07-16 10:41:17 +0200

    Update Funcions_Template.js

--- Sprint_1/Funcions_Template.js
+++ Sprint_1/Funcions_Template.js
@@ -11,16 +11,37 @@ let nom ='Eloi';
 let cognom = 'Isern';
 console.log( `El teu nom és ${ nom } i el cognom ${cognom}`);
 
-//Nivell 2.2 Invocar la funció mitjançant template literals. FALTA ACABAR!!!!!!
+//Nivell 2.2 Invocar la funció mitjançant template literals.
 
-let userName;
-const pasName = (userName)=> console.log(userName);
+let userName; //D'això se'n pot prescindir
+const pasName = (userName)=> userName;
+console.log(`This is ${pasName('Eloi')}`); 
 
-console.log(`This is ${test1('hi there')} and ${test2('hi again')}`); //PER AIXÒ 
 
 //Nivell 3.1 Crea una matriu de deu funcions i empleni-la mitjançant un bucle. Cada funció comptarà del 0-9 imprimint-ho per pantalla. 
 //Invoqui cada funció de l'array iterativament. Haurà d'imprimir-se per pantalla el compte de 0-9 deu vegades
+//Exemple amb 3 funcions, però és exactament igual amb 10.
 
+  function count1() {
+    for (var i = 1; i<10; i++){
+      console.log(i)
+  }};
+  function count2() {
+    for (var i = 1; i<10; i++){
+      console.log(i)
+  }};
+  function count3() {
+    for (var i = 1; i<10; i++){
+      console.log(i)
+  }};
+
+  const array = [count1, count2, count3];
+  
+  for (const fn of array) {
+     fn();
+  };
+
+/*
 let counters = [];
 
 function counter() {
@@ -33,10 +54,11 @@ counter();
 for (i=1; i <10; i++){
   counters.push(counter());
 };
+*/
 
 //Nivell 3.2 Crear una funció anònima autoinvocable (igualada a una variable) que imprimeixi per pantalla el nom d'usuari rebut com a paràmetre
 
-let userName;
-const pasName = (userName)=> console.log(userName);
-
-pasName('Eloi');
+var printConsole = function(userName){
+    console.log(userName);
+};
+printConsole('Eloi');
