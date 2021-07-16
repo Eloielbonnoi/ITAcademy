3e278f850805e1f4c3d0e0cc76a81d75fce4f415
Eloielbonnoi <57134772+Eloielbonnoi@users.noreply.github.com>
2021-07-16 11:55:06 +0200

    Update Arrow_Functions.js

--- Sprint_1/Arrow_Functions.js
+++ Sprint_1/Arrow_Functions.js
@@ -1,38 +1,40 @@
 //Nivell 1
-//mprimir per pantalla el resultat d'una arrow function autoinvocable que sumi dos nombres.
+//Imprimir per pantalla el resultat d'una arrow function autoinvocable que sumi dos nombres. //RESOLT
 
-const sumaArrow = (a,b) => a + b;
-
-console.log(sumaArrow(3, 4));
-
+const sumaArrow = ((a,b) => {
+   console.log(a + b);
+})(3,4);
 
 //Nivell 2
-    //Crear una arrow function que, rebent un paràmetre, retorni un objecte amb un atribut que tingui com a valor el paràmetre rebut.
+//Crear una arrow function que, rebent un paràmetre, retorni un objecte amb un atribut que tingui com a valor el paràmetre rebut.
 
 const arrowFunc = (param) => {
   let spaceship = {  
-    
+      
   'Fuel Type': param ,
   color: 'silver' 
+      
 };
   console.log(spaceship);
       };
 
 arrowFunc('Diesel');
 
-
-    //Crear una classe Persona que rebi un paràmetre 'nom' a l'ésser creat. La classe inclourà un mètode decirNombre que imprimeixi per consola el paràmetre 'Nom'. 
-    //Invocar el mètode decirNombre des de fora de la classe.
+//Nivell 2.2
+//Crear una classe Persona que rebi un paràmetre 'nom' a l'ésser creat. La classe inclourà un mètode decirNombre que imprimeixi per consola el paràmetre 'Nom'. 
+//Invocar el mètode decirNombre des de fora de la classe. //RESOLT
     
 const Persona = class {
 constructor(nom){
 this.nom = nom;
 }
 decirNombre(){
-return this.nom;
+console.log (this.nom);
 }
 };
-console.log(new Persona('Eloi').decirNombre());
+
+const jo = new Persona ('Eloi');
+jo.decirNombre();
 
 
 //Nivell 3
