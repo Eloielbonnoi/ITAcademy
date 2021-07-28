2b604e6259562a30a71fe04b39dc5b7b38ab6d84
Eloielbonnoi <57134772+Eloielbonnoi@users.noreply.github.com>
2021-07-28 12:31:17 +0200

    Update 04_Async_await.js

--- Sprint_1/04_Async_await.js
+++ Sprint_1/04_Async_await.js
@@ -24,9 +24,32 @@ let salaries = [{
     salary: 2000
 }];
 
+const getEmpleado = (id)=> {
+	return new Promise(function(resolve, reject) {
+		const r = employees.find(e => e.id === id);
+		
+    if (r) {
+      console.log('Treballador existent');
+			resolve(r);
+    	} else {
+     	 reject('Treballador desconegut!');
+    		}
+  	})
+	};
+
+const getSalaries =(objEmployee) =>{
+  let result = salaries.find(v =>v.id == objEmployee.id)
+  return result.salary
+};
+
+
 //NIVELL 1.2
-//Creu una funció asíncrona que, rebent un id d'empleat, imprimeixi per pantalla el nom de l'empleat i el seu salari
+//Crea una funció asíncrona que, rebent un id d'empleat, imprimeixi per pantalla el nom de l'empleat i el seu salari
+async 
+
+
+
 //NIVELL 2
-//Creu una funció asíncrona que anomeni a una altra que retorni una Promise que efectuï la seva resolve amb una demora de 2 segons.
+//Crea una funció asíncrona que anomeni a una altra que retorni una Promise que efectuï la seva resolve amb una demora de 2 segons.
 //NIVELL 3
 //Capturi tots els errors possibles del Nivell 2.
