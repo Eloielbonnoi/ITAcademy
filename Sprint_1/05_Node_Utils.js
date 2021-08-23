9d7b168e70eeb886a7faea762d3321e5d3f1e50d
Eloielbonnoi <57134772+Eloielbonnoi@users.noreply.github.com>
2021-08-23 16:04:52 +0200

    Update 05_Node_Utils.js

--- Sprint_1/05_Node_Utils.js
+++ Sprint_1/05_Node_Utils.js
@@ -1,1 +1,18 @@
+//Nivell 1.1
+//Crea una funció que imprimeixi recursivamente un missatge a la consola amb demores d'un segon.//RESOLT
 
+function compteRegressiu (a) {
+        setTimeout(()=>{
+        if (a < 0) return
+        console.log(a)
+        return compteRegressiu(a-1);
+        }, 1000)     
+    };
+
+    compteRegressiu(10);
+
+//Niell 1.2
+//Crea una funció que, en executar-la, escrigui el seu nom en un fitxer.
+
+//Nivell 1.3
+//Crea una altra funció que imprimeixi per pantalla el que llegeixi d'un fitxer.
